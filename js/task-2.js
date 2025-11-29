498dfbfaa00c84f232160f03c77c0012c0caa4e0
Vovk <vovkvaleria06@gmail.com>
2025-11-29 19:02:00 +0200

    fix task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function formatMessage(message, maxLength){
-    if (message.length < maxLength){
+    if (message.length <= maxLength){
         return message;
     } else {
         return message.slice(0, maxLength) + "...";
